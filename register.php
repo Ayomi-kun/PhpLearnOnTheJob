3ac5148234a51b464508f86b26588347812a4f81
Ayomikun Oluwayemi <oluwayemiayo@yahoo.com>
2018-05-08 07:20:12 +0100

    Access denied for username

--- register.php
+++ register.php
@@ -23,3 +23,40 @@
 
 </body>
 </html>
+<?php 
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "first_db";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    
+    // Check connection
+    if ($conn->connect_error) {
+        die("connection failed:" . $conn->connect_error);
+    }
+
+    $username1 = $_POST['username'];
+    $password1 = $_POST['password'];
+
+    $sql = "INSERT INTO users (username, password) Values ('$username1','$password1')";
+
+    if($conn->query(sql) === TRUE) {
+        echo "New Record Created Succesfully";
+    }
+    else{
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->Close();
+    ?>
+
+    
+    // if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // $username = mysql_real_escape_string($_POST['username']);
+    // $password = mysql_real_escape_string($_Post['password']);
+
+    // echo "username entered is = ".$username. "<br />";
+    // echo "password entered is = ".$password. "<br />";
+//   }
