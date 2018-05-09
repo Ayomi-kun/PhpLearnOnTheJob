bd8ab0a4a15a9b76348873f5a0e2eb4fba31847a
Ayomikun Oluwayemi <oluwayemiayo@yahoo.com>
2018-05-09 23:40:03 +0100

    touch - touch

--- checklogin.php
+++ checklogin.php
@@ -13,13 +13,15 @@
         die("connection failed:" . $conn->connect_error);
     }
 
-    $username1 = $_POST['username'];
-    $password1 = $_POST['password'];
+    $username1 =htmlentities($_POST['username']);
+    $password1 = md5($_POST['password']);
 
-    $sql = "INSERT INTO users (username, password) Values ('$username1','$password1')";
+    $sql = "INSERT INTO users (username, password) Values (`$username1`,`$password1`)";
 
     if($conn->query($sql) === TRUE) {
         echo "New Record Created Succesfully";
+        print '<script>alert("Succesfully Registered");</script>';//prompts the user
+        print '<script>window.location.assign("register.php");</script>'; //redirects to register.php
     }
     else{
         echo "Error: " . $sql . "<br>" . $conn->error;
